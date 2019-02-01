e8e12532054ae373d646bf6217c9ff682be26759
Fati CHEN <chen@lirmm.fr>
2019-02-01 20:04:25 +0100

    debugging polygon import

--- dist/spread/sp-ch-a.js
+++ dist/spread/sp-ch-a.js
@@ -3,21 +3,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var d3_polygon_1 = __importDefault(require("d3-polygon"));
+var d3_polygon_1 = require("d3-polygon");
 var lodash_1 = __importDefault(require("lodash"));
 var agora_graph_1 = require("agora-graph");
 // Spread::Convex Hull::Area -- Strobelt 2012 SSS*12
 // TODO: SSS*12
 exports.SpreadConvexHullArea = function (initial, updated) {
-    var initialHull = d3_polygon_1.default.polygonHull(convertNodes(initial.nodes));
-    var updatedHull = d3_polygon_1.default.polygonHull(convertNodes(updated.nodes));
+    var initialHull = d3_polygon_1.polygonHull(convertNodes(initial.nodes));
+    var updatedHull = d3_polygon_1.polygonHull(convertNodes(updated.nodes));
     if (initialHull === null || updatedHull === null)
         return {
             value: -1,
             error: "could not compute initial or updated convex hull"
         };
-    var initialArea = d3_polygon_1.default.polygonArea(initialHull);
-    var updatedArea = d3_polygon_1.default.polygonArea(updatedHull);
+    var initialArea = d3_polygon_1.polygonArea(initialHull);
+    var updatedArea = d3_polygon_1.polygonArea(updatedHull);
     return {
         value: updatedArea / initialArea,
         initial: initialArea,
--- src/spread/sp-ch-a.ts
+++ src/spread/sp-ch-a.ts
@@ -1,4 +1,4 @@
-import d3 from "d3-polygon";
+import { polygonHull, polygonArea } from "d3-polygon";
 import _ from "lodash";
 import { CriteriaFunction, CriteraiResult, Criteria } from "../interfaces";
 import { Node, top, left, right, bottom } from "agora-graph";
@@ -9,8 +9,8 @@ export const SpreadConvexHullArea: CriteriaFunction = function(
   initial,
   updated
 ): CriteraiResult {
-  const initialHull = d3.polygonHull(convertNodes(initial.nodes));
-  const updatedHull = d3.polygonHull(convertNodes(updated.nodes));
+  const initialHull = polygonHull(convertNodes(initial.nodes));
+  const updatedHull = polygonHull(convertNodes(updated.nodes));
 
   if (initialHull === null || updatedHull === null)
     return {
@@ -18,8 +18,8 @@ export const SpreadConvexHullArea: CriteriaFunction = function(
       error: "could not compute initial or updated convex hull"
     };
 
-  const initialArea = d3.polygonArea(initialHull);
-  const updatedArea = d3.polygonArea(updatedHull);
+  const initialArea = polygonArea(initialHull);
+  const updatedArea = polygonArea(updatedHull);
 
   return {
     value: updatedArea / initialArea,
