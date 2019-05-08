2ecc806453267884b994e69eefd26c05c30f58f5
stardisblue <stardisblue@gmail.com>
2019-05-08 18:55:04 +0200

    building dist

--- dist/node-mouvement/dm-custom.d.ts
+++ dist/node-mouvement/dm-custom.d.ts
@@ -1,8 +1,5 @@
-import { Graph, Edge, Point } from 'agora-graph';
-import { Criteria } from '../interfaces';
-export declare function scaleChange(initial: Graph, updated: Graph): {
-    value: number;
-    displacement: Edge<Point>[];
-};
-export declare const NodeMouvementDistanceMovedCustomCriteria: Criteria<"nm_dm_c", "node-mouvement/distance-moved/custom">;
+import { Graph } from 'agora-graph';
+import { CriteriaResult } from '../interfaces';
+export declare function scaleChange(initial: Graph, updated: Graph): CriteriaResult;
+export declare const NodeMouvementDistanceMovedCustomCriteria: import("../interfaces").Criteria<"nm_dm_c", "node-mouvement/distance-moved/custom">;
 export default NodeMouvementDistanceMovedCustomCriteria;
