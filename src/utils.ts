a39d73aa068cbf5dbdbfcc4864056bd3a6b6d988
Fati Chen <chen@lirmm.fr>
2019-10-01 18:11:04 +0200

    correctly throwing an error

--- src/utils.ts
+++ src/utils.ts
@@ -20,9 +20,9 @@ export function criteriaWrap<S extends string, N extends string>({
         'not the same number of nodes' // reason
       ); */
 
-        throw `Criteria ${
-          short ? short : name
-        } abording : not same number of nodes`;
+        throw new Error(
+          `Criteria ${short ? short : name} abording : not same number of nodes`
+        );
       }
       return criteria(initial, updated);
     }
