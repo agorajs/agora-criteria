d107af175a26c0fc4d7ac6690bf93a89c198b041
Fati Chen <chen@lirmm.fr>
2019-10-02 16:25:01 +0200

    :bookmark: :package: 2.0.1

--- dist/change-ratio.d.ts
+++
@@ -1,9 +0,0 @@
-import { CriteriaFunction } from "./interfaces";
-/**
- * TODO check if the edge distance order has changed, apply a levenshtein strategy on it :)
- * @param initialGraph
- * @param updatedGraph
- *
- * @deprecated have no idea of what it is anymore
- */
-export declare const changeRatio: CriteriaFunction;
